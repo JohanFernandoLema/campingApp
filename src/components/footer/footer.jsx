58b4c55a3cd3879a0adab680579cb7c01dd32723
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-22 16:40:18 -0400

    fontawesome brands dependencies added to the react project and website

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,32 @@
 import './footer.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="footerWrapper">
+      <div className="container">
+        <h3>
+          Wood<span>J</span>oy
+        </h3>
+        <div className="socialMedia">
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faGithub} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faInstagram} />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Footer
