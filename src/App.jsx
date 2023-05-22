6df4366913a4af1d96eebac9947f30db94df8710
JohanFernandoLema <johan.13fernando@gmail.com>
2023-05-22 16:25:16 -0400

    footer component created and linked both in scss and jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from './components/navbar/navbar'
 import About from './components/about/about'
 import Carousel from './components/carousel/carousel'
+import Footer from './components/footer/footer'
 export function App() {
   return (
     <div>
       <Navbar />
       <About />
       <Carousel />
+      <Footer />
     </div>
   )
 }
